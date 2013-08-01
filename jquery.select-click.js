f9355df5f8b20fe54877edb3771e7861cd3f641e
Gen <gen@gen-Aspire-V3-551G.(none)>
2013-08-01 12:24:22 +0800

    Add suport to html5

--- jquery.select-click.js
+++ jquery.select-click.js
@@ -1,24 +1,65 @@
 (function ($) {
-  $.fn.selectClick = function (selectCallback, clickCallback, clickTolerance) {
-    var prevX = 0,
-        prevY = 0,
-        clickTolerance = clickTolerance || 2;
+    var DefaultTolerance = 2,
+        ToleranceAttr = 'select-tolerance',
+        SelectAttr = 'select-callback',
+        ClickAttr = 'click-callback',
+        SelectClickKey = 'selectClick';
 
-    this.mousedown(function (e) {
-      prevX = e.pageX;
-      prevY = e.pageY
+    $.fn.selectClick = function (selectCallback, clickCallback, clickTolerance) {
+        var h = {};
+        h[SelectAttr] = selectCallback;
+        h[ClickAttr] = clickCallback;
+        h[ToleranceAttr] = clickTolerance;
+        new SelectClick(this, h)
+    };
+    var SelectClick = function(element, ops){
+        var $element;
+        if(element instanceof jQuery) {
+            $element = element;
+        }else {
+            $element = $($element);
+        }
+        if (!ops) ops = {};
+        $element.data(SelectClickKey, this);
+        this.prevX = ops['prevX'] || 0;
+        this.prevY = ops['prevY'] || 0;
+        var that = this;
+        var mousedown = function(e) {
+            that.prevX = e.pageX;
+            that.prevY = e.pageY;
+        };
+        var click = function(e) {
+            var tolerance = parseInt($element.attr(ToleranceAttr) || ops[ToleranceAttr]);
+            tolerance = isNaN(tolerance) ? 2 : tolerance;
+            var dx = Math.abs(e.pageX - that.prevX);
+            var dy = Math.abs(e.pageY - that.prevY);
+            if (dx < tolerance && dy < tolerance) {
+                eval_t($element.attr(ClickAttr) || ops[ClickAttr])
+            }
+            else {
+                eval_t($element.attr(SelectAttr) || ops[SelectAttr])
+            }
+        };
+        $element.each(function(){
+            this.onmousedown = mousedown;
+            this.onclick = click;
+        });
+    };
+
+    // Add html5 support
+    $(document).delegate('[' + SelectAttr + ']', 'mousedown', function(e){
+        checkSelectClick($(this), {prevX: e.pageX, prevY: e.pageY})
     });
 
-    return this.click(function (e) {
-      var dx = Math.abs(e.pageX - prevX);
-      var dy = Math.abs(e.pageY - prevY);
-      if (dx < clickTolerance && dy < clickTolerance) {
-        clickCallback && clickCallback();
-      }
-      else {
-        selectCallback && selectCallback();
-      }
-    });
-  };
-
+    var checkSelectClick = function(element, ops) {
+        var sc = element.data(SelectClickKey);
+        if(!sc) new SelectClick(element, ops);
+    };
+    var eval_t = function(string) {
+        if (typeof string == 'string') {
+            eval(string);
+        }else if (typeof string == 'function') {
+            string();
+        }
+    }
 })(jQuery);
